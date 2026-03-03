9f719e75eb347edd3c42c0f7c835e185dfaa1f04
bashkirgreg <grgbkrv@gmail.com>
2026-03-03 18:06:48 +0300

    New Hello World

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,7 +1,20 @@
+/*
 #include <iostream>
 using namespace std;
 
 int main() {
     cout << "Hello world!";
+    return 0;
+}
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name;
+    cout << "Please enter your name: ";
+    cin >> name;
+    cout << "Hello world from @" << name << endl;
     return 0;
 }
